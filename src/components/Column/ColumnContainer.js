123b84b53ad7eb8135e42321b033379e6590bdda
Jarek Matwijewicz <alterro@gmail.com>
2021-05-25 20:19:40 +0200

    add search

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,11 +1,13 @@
 import {connect} from 'react-redux';
 import Column from './Column';
+import {getCardsForColumn} from '../../redux/cardsRedux';
+/*
 export const getCardsForColumn = ({cards}, ColumnId) => {
   console.log(cards);
   return cards.filter(card => card.columnId == ColumnId);
   
 };
-
+*/
 const mapStateToProps = (state, props) => ({
   cards: getCardsForColumn(state, props.id),
 });
